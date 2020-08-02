888ba532211254a24772dbe8b36ddd1d7ef65a1a
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-08-02 12:45:57 -0500

    modularize code

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const generatePage = require ('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 const [name, github] = profileDataArgs;
@@ -14,23 +16,10 @@ const [name, github] = profileDataArgs;
 
 // printProfileData(profileDataArgs);
 
-const generagePage = (name, github) => {
-   return `
-    <!DOCTYPE html>
-    <html lang ="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo </title>
-    </head>
-    <body>
-        <h1>${name}</h1>
-        <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-};
 
-console.log(name, github);
-console.log(generagePage(name, github));
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    console.log('Portfolio complete! Checkout index.html to see the output!');
+});
