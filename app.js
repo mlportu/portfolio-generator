68f22e9ac33b2cfb8db53d1e5c6b1b27b07e4c10
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-08-02 20:14:21 -0500

    prompt user input

--- app.js
+++ app.js
@@ -1,14 +1,125 @@
 const inquirer = require('inquirer');
 
-inquirer   
-    .prompt([
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name'
+            message: 'What is your name',
+            validate: nameInput => {
+                if(nameInput){
+                    return true;
+                } else{
+                    console.log('Please eneter your name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username',
+            valdiate: ghName => {
+                if(ghName){
+                    return true;
+                } else{
+                    console.log('Please enter your GitHub username!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
         }
+    ]);
+};
+
+const promptProject = portfiolioData => {
+    if(!portfiolioData.projects){
+        portfiolioData.projects=[];
+    } 
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?',
+            validate: projName => {
+                if(projName) {
+                    return truth;
+                } else {
+                    console.log('Please enter the name of your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)',
+            validate: projDesc => {
+                if(projDesc){
+                    return truth;
+                } else{
+                    console.log('Please enter a project description');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: [ 'JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: ghName => {
+                if (ghName){
+                    return true;
+                }else {
+                    console.log('Please enter the GitHub Link for your project')
+                    return false;
+                }
+            }
+
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+          },
+          {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+          }
     ])
-    .then(answers=>console.log(answers));
+    .then(projectData =>{
+        portfiolioData.projects.push(projectData);
+        if(projectData.confirmAddProject){
+            return promptProject(portfiolioData);
+        }else{
+            return portfiolioData;
+        }
+    });
+};
+
+promptUser()
+    .then(promptProject)
+    .then(portfiolioData => {
+        console.log(portfiolioData);
+    });
+
 // const fs = require('fs');
 // const generatePage = require ('./src/page-template.js');
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
